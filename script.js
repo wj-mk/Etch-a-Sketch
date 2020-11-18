16271678b0058eec781e34447bb5f748a48a551a
wjmk <williamkerley@protonmail.com>
2020-11-18 20:07:30 +0000

    made populate grid function, simplified reset of grid

--- script.js
+++ script.js
@@ -1,72 +1,61 @@
+let gridSize = 16;
+
 // build initial grid
 const grid = document.querySelector('#grid');
 
-let sqNum = 16; // number of squares per row
-let sqLen = 480 / sqNum; // length of side of square
+function populateGrid(size) {
+    for (var i = 0; i < size; i++) {
+        // build row of grid
+        const row = document.createElement('div');
+        row.style.height = `${480/size}px`;
+        // build each square in row
+        for (var j = 0; j < size; j++) {
+            const square = document.createElement('div');
+            square.setAttribute('class', 'square');
+            square.style.height = `${480/size}px`;
+            square.style.width = `${480/size}px`;
+            square.setAttribute('id', `square${i},${j}`);
+            square.setAttribute('onmouseover' , 'mOver(this)')
+            row.appendChild(square);      
+        }
+        grid.appendChild(row)
+    };
+};
 
 // populate grid with squares
-for (var i = 0; i < sqNum; i++) {
-    // build row of grid
-    const row = document.createElement('div');
-    row.style.height = `${sqLen}px`;
-    // build each square in row
-    for (var j = 0; j < sqNum; j++) {
-        const square = document.createElement('div');
-        square.setAttribute('class', 'square');
-        square.style.height = `${sqLen}px`;
-        square.style.width = `${sqLen}px`;
-        square.setAttribute('id', `square${i},${j}`);
-        square.setAttribute('onmouseover' , 'mOver(this)')
-        row.appendChild(square);      
-    }
-    grid.appendChild(row)
-};
+populateGrid(gridSize);
 
 function mOver(obj) {
     obj.style.background = 'pink';
 }
 
 //reset button click
-btn.addEventListener('click', function(e) {
-    // change background of each square to white
-    for (var i = 0; i < sqNum; i++){
-        for (var j = 0; j < sqNum; j++){
-            square = document.getElementById(`square${i},${j}`);
-            square.style.background = 'white';
-        }
-    }  ;
-
-    choice = prompt("How many square per side to make the new grid? (max. 100)");
-    if (choice > 0 || choice <= 100) {
-        sqNum = choice;
-        sqLen = 480/sqNum;
-
-        while (grid.firstChild) {
-            grid.removeChild(grid.firstChild);
-        }
-
-        for (var i = 0; i < sqNum; i++) {
-            // build row of grid
-            const row = document.createElement('div');
-            row.style.height = `${sqLen}px`;
-            // build each square in row
-            for (var j = 0; j < sqNum; j++) {
-                const square = document.createElement('div');
-                square.setAttribute('class', 'square');
-                square.style.height = `${sqLen}px`;
-                square.style.width = `${sqLen}px`;
-                square.setAttribute('id', `square${i},${j}`);
-                square.setAttribute('onmouseover' , 'mOver(this)')
-                row.appendChild(square);      
-            }
-            grid.appendChild(row)
-        };
+btn.addEventListener('click', function() {
+    // reset grid
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
     }
-    
 
+    // obtain new grid size from user
+    while (true) {
+        choice = prompt("How many square per side to make the new grid? (max. 100)");
 
+        console.log(choice)
+        if (choice > 0 && choice <= 100) {
+            gridSize = choice;
+            break;
+        }
+        else if (choice == null) {
+            break;
+        }
+    };
+
+    populateGrid(gridSize)
 });
 
-// standard size grid, but adaptive square size to fit grid
+
+
 // refactor code function to make grid
 // refactor code function to reset grid
+// tidy up code and general refactoring
+// cells going outside grid for some values, I think it's when the inputed size leads to a non-integer quotient.
