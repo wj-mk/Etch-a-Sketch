a9a28d18a4ef026d23b40d6cbb7a705ab6085e28
wjmk <williamkerley@protonmail.com>
2020-11-18 17:28:04 +0000

    size of squares now based on number of squares

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-// build grid
+// build initial grid
 const grid = document.querySelector('#grid');
-for (var i = 0; i < 16; i++) {
+
+let sqNum = 16; // number of squares per row
+let sqLen = 480 / sqNum; // length of side of square
+
+// populate grid with squares
+for (var i = 0; i < sqNum; i++) {
     // build row of grid
     const row = document.createElement('div');
-    row.setAttribute('class', 'row')
+    row.style.height = `${sqLen}px`;
     // build each square in row
-    for (var j = 0; j < 16; j++) {
+    for (var j = 0; j < sqNum; j++) {
         const square = document.createElement('div');
         square.setAttribute('class', 'square');
+        square.style.height = `${sqLen}px`;
+        square.style.width = `${sqLen}px`;
         square.setAttribute('id', `square${i},${j}`);
         square.setAttribute('onmouseover' , 'mOver(this)')
         row.appendChild(square);      
@@ -16,24 +23,22 @@ for (var i = 0; i < 16; i++) {
 }
 
 function mOver(obj) {
-    obj.setAttribute('style', 'background: pink');
+    obj.style.background = 'pink';
 }
 
 //reset button click
 btn.addEventListener('click', function(e) {
     // change background of each square to white
-    for (var i = 0; i < 16; i++){
-        for (var j = 0; j < 16; j++){
+    for (var i = 0; i < sqNum; i++){
+        for (var j = 0; j < sqNum; j++){
             square = document.getElementById(`square${i},${j}`);
-            square.setAttribute('style', 'background: white');
+            square.style.background = 'white';
         }
     }  ;
 
     prompt("How many square per side to make the new grid? (max. 100)")
 });
-    // clear grid
-    // send popup
-        // ask size of new grid
-            // max 100 
-            // number of pixels is constant, size of squares is not
-            
+
+// standard size grid, but adaptive square size to fit grid
+// refactor code function to make grid
+// refactor code function to reset grid
